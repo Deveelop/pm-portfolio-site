721909e5755b734f14207d0744015b7b795b25e1
Victor Abuka <vicsonabuka@gmail.com>
2024-11-18 14:04:08 +0100

    Added second commit

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -1,13 +1,20 @@
 import Button from "./Button"
-
+import Image from "next/image"
 
 const HeroPage = () => {
   return (
-    <section>
-        <p className=" uppercase font-bold">experienced project manager</p>
-        <h1 className="bold-52 lg:bold-88 uppercase">hello i'm Aisha Evelyn</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">I believe that collaboration and creativity are the cornerstones of progress and i deliver successful projects from concept to completion.</p>
-        <Button title="Explore my work" type="button" variant="btn_white border-red-400 " />
+    <section className=" min-h-screen flex items-center px-32">
+      <div className=" flex flex-col md:flex-row items-center gap-8 max-w-5xl">
+      <div className="">
+        <p className=" uppercase font-bold regular-16 text-blue-900">experienced project manager</p>
+        <h1 className="bold-32 lg:bold-40 uppercase">hello i<span className=" text-blue-900">'</span>m <span className="text-blue-900"  >Aisha </span>Evelyn</h1>
+        <p className="regular-14 text-gray-30 lg:max-w-[520px]">I believe that collaboration and creativity are the cornerstones of progress and I deliver successful projects from concept to completion.</p>
+        <Button title="Explore my work" type="button" variant="btn_white mt-3" />
+    </div>
+    <div className="">
+      <Image src='/eve.jpg' alt="professional photo" width={200} height={200} className="rounded-full shadow-lg shadow-blue-900" />
+    </div>
+    </div>
     </section>
   )
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 import Image from "next/image"
 const Nav = () => {
   return (
-    <nav className=" flexEnd max-container padding-container relative z-30 py-5">
+    <nav className=" fixed flexEnd max-container bg-blue-900 text-white padding-container w-full z-30 py-5">
     <ul className=" hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-            <Link className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold" href={link.href} key={link.key}>{link.label}</Link>
+            <Link className="regular-16 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold" href={link.href} key={link.key}>{link.label}</Link>
         ))}
     </ul>
 
